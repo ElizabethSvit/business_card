b8773760394896698c2d6019d5fd71f552aa0721
Elizaveta Svitanko <lizasvit@gmail.com>
2020-09-20 23:21:24 +0300

    mvp

---
+++ src/js/index.js
@@ -0,0 +1,10 @@
+let card = document.querySelector('.full-card');
+card.addEventListener( 'click', function() {
+    card.classList.toggle('is-flipped');
+});
+
+let link_icons = document.getElementsByTagName('a');
+for (let icon of link_icons) {
+    icon.onclick = (event) => event.stopPropagation();
+}
+
